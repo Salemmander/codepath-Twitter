dc1ee646e6ec4c59f3d6fbb10ce3593352a9475f
Salemmander <salemnassar5@gmail.com>
2022-10-08 00:08:53 -0500

    Finished Part 2 of twitter app

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -18,8 +18,12 @@ class HomeTableTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         loadTweets()
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
     }
     
     func loadTweets(){
@@ -66,6 +70,9 @@ class HomeTableTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(isFavorited: tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweet(isRetweeted:tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
